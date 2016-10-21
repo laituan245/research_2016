0bea6e0984166462b9ab64bb28eafd371a9e7d39
Tuan Lai <laituan245@kaist.ac.kr>
2016-10-21 19:56:48 +0900

    Added reset_process.py

---
+++ reset_process.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+
+RAW_DATA_DIRECTORY = 'Raw data'
+PREPROCESSED_DATA_DIRECTORY = 'Preprocessed data'
+GEPHI_DATA_DIRECTORY = 'Gephi data'
+RAW_DATA_FILENAME = 'dblp.txt'
+
+# Let's remove all the intermediate files/folders (if any)
+if os.path.exists(RAW_DATA_DIRECTORY):
+    if os.path.isfile(RAW_DATA_DIRECTORY + '/' + RAW_DATA_FILENAME):
+        os.remove(RAW_DATA_DIRECTORY + '/' + RAW_DATA_FILENAME)
+
+if os.path.exists(PREPROCESSED_DATA_DIRECTORY):
+    shutil.rmtree(PREPROCESSED_DATA_DIRECTORY, ignore_errors=True)
+
+if os.path.exists(GEPHI_DATA_DIRECTORY):
+    shutil.rmtree(GEPHI_DATA_DIRECTORY, ignore_errors=True)
